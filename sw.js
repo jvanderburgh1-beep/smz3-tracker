02a68f18b141cfee8da9c4b06447b74f3592bad0
jvanderburgh1-beep <j.vanderburgh1@gmail.com>
2026-04-24 07:18:51 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@
      speed and offline use.
    ============================================================= */
 
-const CACHE_VERSION = 'smz3-tracker-v17';
+const CACHE_VERSION = 'smz3-tracker-v18';
 const CORE_ASSETS = [
   './',
   './index.html',
